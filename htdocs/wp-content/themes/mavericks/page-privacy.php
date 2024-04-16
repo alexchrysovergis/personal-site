b225b8b950ffed10714b8830bf79b354d3ea4eb5
alexchrysovergis <alexandroschrysovergis@gmail.com>
2024-04-16 11:56:53 -0700

    Add privacy and contact page template

---
+++ htdocs/wp-content/themes/mavericks/page-privacy.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section class="privacy-section container-fluid px-5 py-3 text-light">
+	<div class="row">
+		<div class="col-12">
+        <?php the_content(); ?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
